71b2ff9ff7c4a73854388bba252d60639c9d1982
Bradley Willcott <bw.opensource@yahoo.com>
2020-11-26 15:24:17 +0800

    changes to POMProperties and rename markdownj-cli.properties to pom.properties. Signed-off-by:Bradley Willcott <bw.opensource@yahoo.com>

--- src/main/java/org/markdownj/cli/POMProperties.java
+++ src/main/java/org/markdownj/cli/POMProperties.java
@@ -26,11 +26,12 @@ import java.util.Properties;
  * Provides access to some of the projects pom.properties.
  * <p>
  * To access the properties:
- * </p>
+ * <hr>
  * <pre><code>
  * POMProperties pom = POMProperties.INSTANCE;
  * System.out.println(pom.title):
  * </code></pre>
+ * <hr>
  *
  * @author <a href="mailto:bw.opensource@yahoo.com">Bradley Willcott</a>
  *
@@ -40,10 +41,44 @@ import java.util.Properties;
 public final class POMProperties {
 
     /**
+     * Set this to the file you are using.
+     * <p>
+     * Source location of file is usually: {@code ${basedir}/src/main/resources/}
+     * <p>
+     * Configure your {@code pom.xml} file:
+     * <hr>
+     * <pre><code>
+     *    &lt;resources&gt;
+     *        &lt;resource&gt;
+     *           &lt;directory&gt;src/main/resources&lt;/directory&gt;
+     *           &lt;filtering&gt;true&lt;/filtering&gt;
+     *           &lt;includes&gt;
+     *               &lt;include&gt;**&#47;pom.properties&lt;/include&gt;
+     *           &lt;/includes&gt;
+     *       &lt;/resource&gt;
+     *       &lt;resource&gt;
+     *           &lt;directory&gt;src/main/resources&lt;/directory&gt;
+     *           &lt;filtering&gt;false&lt;/filtering&gt;
+     *           &lt;excludes&gt;
+     *               &lt;exclude&gt;**&#47;pom.properties&lt;/exclude&gt;
+     *           &lt;/excludes&gt;
+     *       &lt;/resource&gt;
+     *   &lt;/resources&gt;
+     * </code></pre>
+     * <hr>
+     */
+    public static final String PROPERTIES_FILENAME = "/pom.properties";
+
+    /**
      * Provides single instance of this class.
      */
-    public final static POMProperties INSTANCE = new POMProperties();
+    public static final POMProperties INSTANCE = new POMProperties();
 
+    /**
+     * For testing purposes.
+     *
+     * @param args Ignored.
+     */
     public static void main(String[] args) {
         System.out.println(POMProperties.INSTANCE);
     }
@@ -85,7 +120,7 @@ public final class POMProperties {
         Properties properties = new Properties();
         try
         {
-            properties.load(POMProperties.class.getResourceAsStream("/markdownj-cli.properties"));
+            properties.load(POMProperties.class.getResourceAsStream(PROPERTIES_FILENAME));
         } catch (IOException ex)
         {
             throw new RuntimeException("FileIOError", ex);
@@ -104,9 +139,9 @@ public final class POMProperties {
         return new StringBuilder(POMProperties.class.getName()).append(":\n")
                 .append("  title: ").append(title).append("\n")
                 .append("  description: ").append(description).append("\n")
-                .append("  version: ").append(version).append("\n")
+                .append("  groupId: ").append(groupId).append("\n")
                 .append("  artifactId: ").append(artifactId).append("\n")
-                .append("  groupId: ").append(groupId).append("\n").toString();
+                .append("  version: ").append(version).append("\n")
+                .append("  filename: ").append(filename).append("\n").toString();
     }
-
 }
